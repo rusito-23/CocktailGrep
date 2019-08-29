2f902aeea6be2efadf87c24afb64b637a7d707e0
Igor Andruskiewitsch <igor.andruskiewitsch@incluit.com>
2019-08-29 15:16:33 -0300

    Initial Commit

---
+++ SomeCocktail/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SomeCocktail
+//
+//  Created by Igor Andruskiewitsch on 8/29/19.
+//  Copyright © 2019 Rusito23. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
