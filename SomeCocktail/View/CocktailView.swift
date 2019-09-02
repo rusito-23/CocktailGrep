68abdf7029d8f0f6c816e139e44ae54366e892f3
Igor Andruskiewitsch <igor.andruskiewitsch@incluit.com>
2019-09-02 10:04:38 -0300

    Fix image fill into table row

--- SomeCocktail/View/CocktailView.swift
+++ SomeCocktail/View/CocktailView.swift
@@ -18,15 +18,12 @@ struct CocktailView: View {
         return VStack(alignment: .leading, spacing: 10) {
             Text(cocktail.name).font(.largeTitle)
             Text("(\(cocktail.alcoholic ?? "unknown"))").font(.title)
-        }.background(
+        }.frame(minWidth: 0, maxWidth: .infinity,
+                minHeight: 0, maxHeight: 200, alignment: .topLeading)
+        .background(
             Image(uiImage: imageModel.image)
                 .resizable()
                 .scaledToFill()
-                .allowsHitTesting(false)
-                .clipped()
         ).cornerRadius(10)
-            .frame(minWidth: 0, idealWidth: 0, maxWidth: .infinity,
-                   minHeight: 0, idealHeight: 150, maxHeight: 150,
-                   alignment: .topLeading)
     }
 }
